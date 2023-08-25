5ffb85f50aa04a73d8d62f1c22c3a1512efc3368
dineshmic <dinesh.mic@gmail.com>
2023-08-25 21:34:00 +0530

    for date script using oai

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,23 @@
-const { DateTime } = require("luxon");
+const { DateTime } = require('luxon'); // Import Luxon's DateTime module
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets');
-	eleventyConfig.addPassthroughCopy('./src/admin');
-	eleventyConfig.addFilter("postDate", (dateObj) => {
-		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-	});
-	
-	
-	return {
-		dir: {
-			input: "src",
-			output: "public"
-		}
-	};
+    eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        // Remove single quotes from the dateObj string
+        const dateStringWithoutQuotes = dateObj.replace(/'/g, '');
+        
+        // Parse the date string using Luxon's DateTime
+        const parsedDate = DateTime.fromJSDate(new Date(dateStringWithoutQuotes));
+        
+        // Format the parsed date as a localized date string
+        return parsedDate.toLocaleString(DateTime.DATE_MED);
+    });
+    
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
 }
